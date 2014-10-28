89ac8cac44170ef69ca61da2f0ecc6dc34789e05
Patrick Arsenio <patrickarsenio@hotmail.com>
2014-10-28 10:36:21 +0000

    3ª alteracao

--- ola.cpp
+++ ola.cpp
@@ -1,5 +1,5 @@
-#include<s t d i o . h>
-int main ( ) {
-p r i n t f ( ” ol a mundo\n” ) ;
-return 1 ;
-}
+#include<s t d i o . h>
+int main ( ) {
+p r i n t f ( ” TPPI RULES! ! \ n” ) ;
+return 1 ;
+}
